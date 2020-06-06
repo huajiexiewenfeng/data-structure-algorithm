0a4cf34d45a6d2679da00daa6fc4709d06cb807b
xwf <420842659@qq.com>
2020-06-07 07:13:26 +0800

    递归

--- src/main/java/com/huajie/chapter06/StepsRecursionDemo.java
+++ src/main/java/com/huajie/chapter06/StepsRecursionDemo.java
@@ -5,6 +5,9 @@ public class StepsRecursionDemo {
         int n = 10;
         int steps = f(n);
         System.out.printf("[%d]阶楼梯，我们有[%d]种走法\n", n, steps);
+        System.out.println("====改写之后====");
+        steps = f2(n);
+        System.out.printf("[%d]阶楼梯，我们有[%d]种走法\n", n, steps);
     }
 
     private static int f(int n) {
@@ -24,14 +27,14 @@ public class StepsRecursionDemo {
         if (n == 2) {
             return 2;
         }
-
         int res = 0;
-//        int n_1 = 1;
-//        int n_2 = 2;
-//        for (int i = 0; i < n; i++) {
-//            res = n_1 + n_2;
-//        }
-
+        int n_minus_1 = 2;// 假设 n=3,n-1=2 个台阶有2种走法
+        int n_minus_2 = 1;// 假设 n=3,n-2=1 个台阶有1种走法
+        for (int i = 3; i <= n; i++) {
+            res = n_minus_1 + n_minus_2;
+            n_minus_2 = n_minus_1;// 假设 n=4,第二次循环，4-2 = 2 有2种走法
+            n_minus_1 = res;// 假设 n=4,第二次循环，4-1=3 有 3(上一次的2+1) 种走法
+        }
         return res;
     }
 }
