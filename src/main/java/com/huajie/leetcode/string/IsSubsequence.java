c5047bc30ef336349db17c8380612190a2ec80b4
xwf <420842659@qq.com>
2020-07-27 22:05:17 +0800

    LeetCode 练习每日一题

---
+++ src/main/java/com/huajie/leetcode/string/IsSubsequence.java
@@ -0,0 +1,47 @@
+package com.huajie.leetcode.string;
+
+/**
+ * 392. 判断子序列
+ * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ * <p>
+ * 你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
+ * <p>
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+ * <p>
+ * 示例 1:
+ * s = "abc", t = "ahbgdc"
+ * <p>
+ * 返回 true.
+ * <p>
+ * 示例 2:
+ * s = "axc", t = "ahbgdc"
+ * <p>
+ * 返回 false.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/is-subsequence
+ *
+ * @author ：xwf
+ * @date ：Created in 2020\7\27 0027 21:59
+ */
+public class IsSubsequence {
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbgdc";
+        System.out.println(isSubsequence(s,t));
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        int n = s.length();
+        int m = t.length();
+        while (i < n && j < m) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == n;
+    }
+}
